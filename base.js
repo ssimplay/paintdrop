805aab98a682b62d275a4841d9639f77b4cd9fb9
ssimplay <46297508+ssimplay@users.noreply.github.com>
2020-04-30 13:20:57 +0900

    ver5.4
    다른파일 올려서 수정

--- base.js
+++ base.js
@@ -1,101 +1,24 @@
 $(function(){
-    
-    
-    /* 동작 */
-    $(".w_btn li").click(function(){
-       
-        $(".w_inner").animate({ marginLeft:-1280*$(this).index() });
-        
-        
-        /* 버튼 색상 */
-        $(".w_btn li button").css({ background:"#fff2d8" , color:"#065471" });
-        $(".w_btn li:eq("+$(this).index()+") button").css({ background:"#065471" , color:"#ffc045" });
-        
-        
-        $(".w_inner .hide1").fadeOut();
-        $(".w_inner .hide2").fadeOut();
-        $(".w_inner .btn").fadeIn();
-        
-    });
-    
-    
-    
-    
-    
    
-    /* 더보기1 */
-   $(".w_inner li #btn1").click(function(){
-      
-       $(".w_inner #btn1").fadeOut();
-       $(".w_inner .hide1").fadeIn();
-      
-       
-   }); 
-    
-    /* 더보기2 */
-    $(".w_inner li #btn2").click(function(){
-      
-       $(".w_inner #btn2").fadeOut();
-       $(".w_inner .hide2").fadeIn();
-      
-       
-   }); 
-    
-    
-    
-    
-    /* tablet */
-var dw = document.documentElement.clientWidth;
-    
-    
-
-    if(dw < 1298 ){
-       
+    /* 햄버거메뉴 클릭 */
+    $('.mnb button').click(function(){
         
-        /* portfolio */
-        $(".w_btn li").click(function(){
-            
-            
-            var uw = $(".w_inner ul").width();
-            
-               
-            $(".w_inner").stop().animate({ marginLeft:-(uw*$(this).index()) });
-                
+        $(this).fadeOut();
+        $('.mnb_inner').animate({
+            marginRight : 0
+        }, 1000 );
         
-        });
-        
-    };
+    })
     
-    if(dw < 600 ){
-       
-       
-        $("nav button").on('click',function(){
-           
-            $('.gnb').fadeIn(1500);
-            $("nav button").fadeOut();
-            
-        });
+    /* 닫기 버튼 클릭 */
+    $('.exit').click(function(){
         
+        $('.mnb button').fadeIn();
+        $('.mnb_inner').animate({
+            marginRight : '-30%'
+        }, 1000 );
         
-        $(".exit").click(function(){
-           
-            $(".gnb").fadeOut(1500);
-            $("nav button").fadeIn();
-            
-            
-        });
-        
-    };
-    
-    
+    })
     
     
 });
-
-
-
-
-
-
-
-
